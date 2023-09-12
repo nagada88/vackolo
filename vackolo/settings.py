cdd60ee7d1b06c859fadc69e507482aa0f016322
Adam Nagy <nagada88@gmail.com>
2023-09-12 21:51:29 +0200

    first commit of vackolo

--- vackolo/settings.py
+++ vackolo/settings.py
@@ -32,7 +32,7 @@ LOCALE_PATHS = [
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
